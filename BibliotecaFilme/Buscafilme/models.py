6cde2a07276e29b92be601b2ba3e8876152a14d6
Luan-kennedy <luan.kennedy.silva.ls@gmail.com>
2025-03-02 02:26:45 -0300

    ⬆️ versão banco

--- BibliotecaFilme/Buscafilme/models.py
+++ BibliotecaFilme/Buscafilme/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 class Ator(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100,primary_key=True )
     data_nascimento = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.nome
 
 class Filme(models.Model):
-    titulo = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=200, primary_key=True)
     ano = models.IntegerField()
     diretor = models.CharField(max_length=100)
-    atores = models.ManyToManyField(Ator)
+    atores = models.ManyToManyField(Ator,related_name='filmes')
     sinopse = models.TextField()
-
+    
     def __str__(self):
         return self.titulo
